64c177ce56463902b41565f63371b81eef557dd3
Aditya DS <adithya18062000@gmail.com>
2021-01-06 01:31:18 +0530

    initial commit

---
+++ fylo.js
@@ -0,0 +1,24 @@
+window.addEventListener("resize", function() {
+  if (window.innerWidth < 768) {
+    document.querySelectorAll("button")[2].classList.add("form-control");
+    document.querySelector("#working div").classList.add("info");
+  }
+  if (window.innerWidth >= 768) {
+    document.querySelectorAll("button")[2].classList.remove("form-control");
+  }
+  if (window.innerWidth >= 768 && window.innerWidth <= 992) {
+    document.querySelector("#contact").classList.add("text-right");
+    document.querySelector("#links").classList.add("text-right");
+  }
+  else if (window.innerWidth < 768 || window.innerWidth > 992){
+    document.querySelector("#contact").classList.remove("text-right");
+    document.querySelector("#links").classList.remove("text-right");
+  }
+});
+
+// window.onload = function() {
+  if (window.innerWidth >= 768 && window.innerWidth <= 992) {
+    document.querySelector("#contact").classList.add("text-right")
+    document.querySelector("#links").classList.add("text-right")
+  }
+// }
